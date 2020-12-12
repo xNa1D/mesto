251608ab81ac2b4b2bc8fdfda8b599380356c93b
xNa1D <moskalev@mindbox.ru>
2020-12-12 13:31:41 +0300

    feat: cards from massive

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,45 @@ let title = document.querySelector('.profile-info__title');
 let nameInput = document.getElementsByName('profile-edit-name')[0];
 let titleInput = document.getElementsByName('profile-edit-title')[0];
 
+const cardsContainer = document.querySelector('.cards');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+initialCards.forEach(function(entry) {
+    const cardTemplate = document.querySelector('#cards-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.cards__title').textContent = entry.name;
+    cardElement.querySelector('.cards__image').src = entry.link;
+    cardElement.querySelector('.cards__image').alt = entry.name;
+
+    cardsContainer.append(cardElement);
+});
+
 function openEditPopup() {
     titleInput.value = title.textContent;
     nameInput.value = name.textContent;
