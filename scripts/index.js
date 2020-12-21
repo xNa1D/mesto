8b0d7ef835fffcf39ee296d1d79631fa87e6f014
xNa1D <moskalev@mindbox.ru>
2020-12-21 22:59:07 +0300

    feat: validation html/css

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 const profileEditBtn = document.querySelector('.profile__edit-button');
 const cardAddBtn = document.querySelector('.profile__add-button');
 
-const popupEditForm = document.getElementsByName('profile-edit-form')[0];
-const popupAddCardForm = document.getElementsByName('profile-add-card-form')[0];
-const popupImgForm = document.getElementsByName('img-form')[0];
+const popupEditForm = document.forms['profile-edit-form'];
+const popupAddCardForm = document.forms['profile-add-card-form'];
+const popupImgForm = document.forms['img-form'];
 
 const popupCloseBtns = document.querySelectorAll('.popup__close');
 
 const name = document.querySelector('.profile-info__name');
 const title = document.querySelector('.profile-info__title');
 
-const nameInput = document.getElementsByName('profile-edit-name')[0];
-const titleInput = document.getElementsByName('profile-edit-title')[0];
+const nameInput = popupEditForm.elements['profile-edit-name'];
+const titleInput = popupEditForm.elements['profile-edit-title'];
 
-const cardNameInput = document.getElementsByName('card-name')[0];
-const cardImgInput = document.getElementsByName('card-img')[0];
+const cardNameInput = popupAddCardForm.elements['card-name'];
+const cardImgInput = popupAddCardForm.elements['card-img'];
 
 const cardsContainer = document.querySelector('.cards');
 
---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+// function enableValidation({
+//     formSelector: '.popup__form',
+//     inputSelector: '.popup__input-text',
+//     submitButtonSelector: '.popup__button-save',
+//     inactiveButtonClass: 'popup__button_disabled',
+//     inputErrorClass: 'popup__input_type_error',
+//     errorClass: 'popup__error_visible'
+//   }) {};
